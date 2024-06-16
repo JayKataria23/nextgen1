29db83c3346ef5f7f5997c87e2b1d1cd0de4c68a
JayKataria23 <66278443+JayKataria23@users.noreply.github.com>
2024-06-17 01:37:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import datetime
 from st_supabase_connection import SupabaseConnection, execute_query
+import smtplib
+from email.message import EmailMessage
 
 conn = st.connection("supabase",type=SupabaseConnection)
 
@@ -25,13 +27,32 @@ with st.form("form"):
             
             st.success("Form submitted successfully!")
 
+            msg = EmailMessage()
+            subject = "Patch Report "+location+" "+status
+            sender = "imbuzixjay@gmail.com"
+            to = ["nxtgen@inmac.co.in","jaykataria2004@gmail.com"]
+            password = "aehl bovs lfaj lybs"
+            msg = EmailMessage()
+            msg['Subject'] = subject
+            msg['From'] = sender
+            content = "New patch reported created\n\n\n\nEngineer: "+engineer_name+"\n\nEngineer Contact Number: "+engineer_contact_number+"\n\nEngineer Email: "+engineer_email+"\n\nLocation: "+location+"\n\nLocation Contact Number: "+location_contact_number+"\n\nLocation Contact Person: "+location_contact_person+"\n\nStatus: "+status+"\n\nImage URL's: "
+            msg.set_content(content)
             images = []
             if image is not None:
                 for i in image:
                     filename = "nextgen1/"+str(datetime.datetime.now())+i.__getattribute__("name")
                     conn.upload("images", "local",i , filename)
                     images.append(filename)
-            
+                    st.write(i.__getattribute__("name").split('.')[-1])
+                    msg.add_attachment(i.read(), maintype='image', subtype=i.__getattribute__("name").split('.')[-1])
+
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+                smtp.login(sender, password)
+                msg['to'] = to
+                smtp.send_message(msg)
+                st.success("Email Sent!")
+
+
             execute_query(conn.table('nextgen1').insert([{"engineer_name":engineer_name,
                                                           "engineer_contact_number":engineer_contact_number,
                                                           "engineer_email":engineer_email,
