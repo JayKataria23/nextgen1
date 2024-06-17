23b4fb5ce40f3a19c24e835f9e82b96a7a227636
JayKataria23 <66278443+JayKataria23@users.noreply.github.com>
2024-06-17 15:35:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ with st.form("form"):
             msg = EmailMessage()
             subject = "Patch Report "+location+" "+status
             sender = "imbuzixjay@gmail.com"
-            to = ["nxtgen@inmac.co.in","jaykataria2004@gmail.com"]
+            to = ["nxtgen@inmac.co.in"]
             password = "aehl bovs lfaj lybs"
             msg = EmailMessage()
             msg['Subject'] = subject
